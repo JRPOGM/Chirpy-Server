83367d91fac08f08178655bdc60b836969cf8c4b
JRPOGM <jrmacriss@gmail.com>
2026-08-27 10:25:51 -0700

    built a handler function

--- main.go
+++ main.go
@@ -8,11 +8,18 @@ import (
 func main() {
 	const port = "8080"
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(".")))
+	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
+	mux.HandleFunc("/healthz", handlerReadiness)
 	srv := &http.Server{
 		Addr:		":" + port,
 		Handler: mux,
 	}
 	log.Printf("Serving on port: %s\n", port)
 	log.Fatal(srv.ListenAndServe())
+}
+
+func handlerReadiness(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
 }
