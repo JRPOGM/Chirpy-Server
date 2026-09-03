f0721b9e7a4d13ffdc94220379d8f66cb3cf1d1b
JRPOGM <jrmacriss@gmail.com>
2026-09-03 12:45:20 -0700

    fixing some bugs

--- main.go
+++ main.go
@@ -38,7 +38,7 @@ func main() {
 	mux.HandleFunc("GET /admin/metrics", apiCfg.handlerMetrics)
 	mux.HandleFunc("POST /admin/reset", apiCfg.handlerReset)
 	mux.HandleFunc("POST /api/validate_chirp", handlerValidateChirp)
-	mux.HandleFunc("POST /api/users", handlerUsers)
+	mux.HandleFunc("POST /api/users", apiCfg.handlerUsers)
 	srv := &http.Server{
 		Addr:		":" + port,
 		Handler: mux,
