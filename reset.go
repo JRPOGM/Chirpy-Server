848c53854829b6257492e4a6286b519b3e37a657
JRPOGM <jrmacriss@gmail.com>
2026-09-03 12:47:16 -0700

    fixed responding with error

--- reset.go
+++ reset.go
@@ -6,13 +6,13 @@ import (
 
 func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
 	if cfg.platform != "dev" {
-		respondWithError(w, http.StatusForbidden, "forbidden")
+		respondWithError(w, http.StatusForbidden, "forbidden", nil)
 		return
 	}
 	cfg.fileserverHits.Store(0)
 	err := cfg.db.ResetUsers(r.Context())
 	if err != nil {
-		respondWithError(w, http.StatusInternalServerError, "couldn't reset users")
+		respondWithError(w, http.StatusInternalServerError, "couldn't reset users", err)
 		return
 	}
 	respondWithJson(w, http.StatusOK, struct{}{})
