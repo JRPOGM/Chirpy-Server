47ffc490a74ddec9585aa40e11f69d2267608ad9
JRPOGM <jrmacriss@gmail.com>
2026-09-03 15:26:45 -0700

    making an array

--- handler_chirp_getter.go
+++ handler_chirp_getter.go
@@ -5,10 +5,20 @@ import (
 )
 
 func (cfg *apiConfig) handlerGetChirps(w http.ResponseWriter, r *http.Request) {
-	chirp, err := cfg.db.GetChirps(r.Context())
+	dbChirp, err := cfg.db.GetChirps(r.Context())
 	if err != nil {
 		respondWithError(w, http.StatusInternalServerError, "Couldn't find the chirps", err)
 		return
 	}
-	respondWithJson(w, http.StatusOK, chirp)
+	chirps := []Chirp{}
+	for _, chirp := range dbChirp {
+		chirps = append(chirps, Chirp{
+			ID: chirp.ID,
+			CreatedAt: chirp.CreatedAt,
+			UpdatedAt: chirp.UpdatedAt,
+			Body: chirp.Body,
+			UserID: chirp.UserID,
+		})
+	}
+	respondWithJson(w, http.StatusOK, chirps)
 }
