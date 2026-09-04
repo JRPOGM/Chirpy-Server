54f2757fa09f0bd0e238190f2fcaa28f20aec74a
JRPOGM <jrmacriss@gmail.com>
2026-09-04 11:09:03 -0700

    adding jwt to my auth

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -1,7 +1,17 @@
 package auth
 
 import (
+	"time"
+	"errors"
+	"fmt"
+	"github.com/google/uuid"
 	"github.com/alexedwards/argon2id"
+	"github.com/golang-jwt/jwt"
+)
+type TokenType string
+
+const (
+	TokenTypeAccess TokenType = "chirpy-access"
 )
 
 func HashPassword(password string) (string, error) {
@@ -10,4 +20,43 @@ func HashPassword(password string) (string, error) {
 
 func CheckPasswordHash(password, hash string) (bool, error) {
 	return argon2id.ComparePasswordAndHash(password, hash)
+}
+
+func MakeJWT(userID uuid.UUID, tokenSecret string. expiresIn time.Duration) (string, error) {
+	signingKey := []byte(tokenSecret)
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.RegisteredClaims{
+		Issuer:		string(TokenTypeAccess),
+		IssuedAt:	jwt.NewNumericDate(time.Now().UTC()),
+		ExpiresAt:	jwt.NewNumericDate(time.Now().UTC().Add(expiresIn)),
+		Subject:	userID.String(),
+	})
+	return token.SignedString(signingKey)
+}
+
+func ValidateJWT(tokenString, tokenSecret string) (uuid.UUID, error) {
+	claimsStruct := jwt.RegisteredClaims{}
+	token, err := jwt.ParseWithClaims(
+		tokenString,
+		&claimsStruct,
+		func(token *jwt.Token) (interface{}, error) { return []byte(tokenSecret), nil},
+	)
+	if err != nil {
+		return uuid.Nil, err
+	}
+	userIDString, err := token.Claims.GetSubject()
+	if err != nil {
+		return uuid.Nil, err
+	}
+	issuer, err := token.Cialms.GetIssuer()
+	if err != nil {
+		return uuid.Nil, err
+	}
+	if issuer != string(TokenTypeAccess) {
+		return uuid.Nil, errors.New("invalid issuer")
+	}
+	if, err := uuid.Parse(userIDString)
+	if err != nil {
+		return uuid.Nil, fmt.Errorf("invalid user ID: %w", err)
+	}
+	return id, nil
 }
