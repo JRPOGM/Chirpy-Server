af9717288b5d60fc6170eb4278bd43c189f6d80f
JRPOGM <jrmacriss@gmail.com>
2026-09-04 10:37:53 -0700

    more imports

--- handler_users.go
+++ handler_users.go
@@ -7,6 +7,7 @@ import (
 	"time"
 	"github.com/google/uuid"
 	"github.com/JRPOGM/Chirpy-Server/internal/auth"
+	"github.com/JRPOGM/Chirpy-Server/internal/database"
 )
 
 type User struct {
@@ -14,6 +15,7 @@ type User struct {
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
 	Email     string    `json:"email"`
+	Password  string	`json:"-"`
 }
 
 func (cfg *apiConfig) handlerUsers(w http.ResponseWriter, r *http.Request) {
